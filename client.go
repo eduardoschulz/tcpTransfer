eda3ded8238cb676991c79b43301a7192a0b0a96
Eduardo Schulz <eduardo.schulz@protonmail.com>
2023-11-11 09:14:19 -0300

    commit1

---
+++ client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+
+func main() {
+  conn, err := net.Dial("tcp", "localhost:9999")
+  
+  if err != nil {
+    log.Println(err)
+    return
+  }
+
+  //for {
+  
+       // pid := os.Getppid()
+       // pidStr := strconv.Itoa(pid)
+  
+ //       conn.Write([]byte(pidStr))
+        //write a sleep for 10 seconds
+
+        file, err := os.Open("test.jpg")
+        if err != nil {
+          log.Fatal(err)
+        }
+
+        fi,_ := file.Stat()
+        defer file.Close()
+
+        // get the file size
+        size := fi.Size()
+        println("File size:", size)
+
+
+        data := make([]byte, size) 
+        
+
+        count, err := file.Read(data)
+        
+
+        if err != nil {
+          log.Fatal(err)
+        }
+        //divide the file into 600 parts
+        for i := 0; i < 600; i++ {
+          conn.Write(data[i*count/600:(i+1)*count/600])
+          time.Sleep(100 * time.Millisecond)
+        }
+
+
+
+
+        //conn.Write(data[:count])
+}
+
---
+++ server.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+
+func main() {
+  handleConnection()
+
+}
+
+
+func handleConnection() {
+
+  listener, err := net.Listen("tcp", ":9999")
+
+  if err != nil {
+    log.Println(err)
+    return
+  }
+
+  defer listener.Close()
+  fmt.Println("Listening on port 9999")
+
+  for {
+    conn, err := listener.Accept()
+    if err != nil {
+      log.Println(err)
+      continue
+    }
+    
+    go handleClient(conn)
+
+
+
+}
+}
+
+func handleClient(conn net.Conn){
+  //defer conn.Close()
+  fmt.Println("Client connected from " + conn.RemoteAddr().String())
+ // for {
+   /* 
+    var buf [2701662]byte cria buffer que vai armazenar a mensagem recebida pelo socket 
+    n, err := conn.Read(buf[0:])
+
+    if err != nil {    verifica se ocorreu algum erro na leitura da mensagem 
+      log.Println(err) 
+      return
+    }
+    s := string(buf[0:n])  converte o buffer em string 
+    fmt.Println("Received:", s)
+    _, err2 := conn.Write([]byte("Received: "+ conn.LocalAddr().String()))
+    if err2 != nil {
+      log.Println(err2)
+      return
+    }
+    */
+    f, err := os.Create("received.jpg") /* cria um arquivo para armazenar a mensagem recebida */
+    if err != nil { /* verifica se ocorreu algum erro na criação do arquivo */
+      log.Fatal(err) 
+    }
+    var buf []byte
+    conn.Read(buf[0:]) /* lê a mensagem recebida pelo socket */
+
+    f.Write(buf)/* escreve o stream de dados recebida no arquivo */
+    f.Close() /* fecha o arquivo */
+ // }
+
+}
